8735097cc8185c0ec7d423243d009b3c9f9ffd9f
alumno <alumno@utnfra.com.ar>
2020-02-13 19:50:50 -0300

    ejercicio 4 tp

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -4,21 +4,22 @@ function mostrar() {
 	var max;
 	var min;
 
-	max = 11;
+	max = 11; // no toma el maximo valor que deseamos se le suma 1
 	min = 1;
 	numeroRandom = Math.floor((Math.random() * (max - min)) + min);
 	console.log(numeroRandom);
 
-	if (numeroRandom >= 9 && numeroRandom == 10) {
-		alert('Excelente');
-	}	
-	else {
-		if (numeroRandom >= 4 && numeroRandom <= 8) {
+	if (numeroRandom >= 9) {
 		alert('Excelente');
 	}
-	else { alert('Vamos, la proxima se puede')
+	else {
+		if (numeroRandom >= 4) {
+			alert('Aprobo');
+		}
+		else {
+			alert('Vamos, la proxima se puede')
 		}
 	}
-	
-	
+
+
 }//FIN DE LA FUNCIÓN
